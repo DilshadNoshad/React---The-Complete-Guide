cfefd69702c0de3492352f04914ae9ae09bd76b6
DilshadSheikh <dilshadnoshad2003@gmail.com>
2022-07-19 00:29:12 +0500

    upload files

---
+++ Realistic Custom Hook/src/components/NewTask/NewTask.js
@@ -0,0 +1,37 @@
+import useHttp from "../../hooks/use-http";
+import Section from "../UI/Section";
+import TaskForm from "./TaskForm";
+
+const NewTask = (props) => {
+  const { isLoading, error, sendRequest: sendTasksRequest } = useHttp();
+
+  const createTasks = (taskText, taskData) => {
+    const generatedId = taskData.name; // firebase-specific => "name" contains generated id
+    const createdTask = { id: generatedId, text: taskText };
+
+    props.onAddTask(createdTask);
+  };
+
+  const enterTaskHandler = async (taskText) => {
+    sendTasksRequest(
+      {
+        url: "https://react-http-8a166-default-rtdb.firebaseio.com/tasks.json",
+        method: "POST",
+        body: { text: taskText },
+        headers: {
+          "Content-Type": "application/json",
+        },
+      },
+      createTasks.bind(null, taskText)
+    );
+  };
+
+  return (
+    <Section>
+      <TaskForm onEnterTask={enterTaskHandler} loading={isLoading} />
+      {error && <p>{error}</p>}
+    </Section>
+  );
+};
+
+export default NewTask;
