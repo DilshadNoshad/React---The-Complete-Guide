8de1ff173f1b53a958f5a434d3be0f26ab0f0ba5
DilshadSheikh <dilshadnoshad2003@gmail.com>
2022-09-07 02:19:29 +0500

    update

--- food ordering/src/components/Meals/AvailableMeals.js
+++ food ordering/src/components/Meals/AvailableMeals.js
@@ -11,7 +11,7 @@ const AvailableMeals = () => {
   useEffect(() => {
     const fetchMealApi = async () => {
       const response = await fetch(
-        "https://react-http-8a166-default-rtdb.firebaseio.com/meals.json"
+        "yourapi/meals.json"
       );
 
       if (!response.ok) {
