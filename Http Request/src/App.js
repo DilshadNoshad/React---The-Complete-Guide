38b926b87e509424bc000b7fe8d4c5725c822539
DilshadSheikh <dilshadnoshad2003@gmail.com>
2022-09-07 02:14:28 +0500

    update

--- Http Request/src/App.js
+++ Http Request/src/App.js
@@ -14,7 +14,7 @@ function App() {
     setError(null);
     try {
       const response = await fetch(
-        "https://react-http-8a166-default-rtdb.firebaseio.com/movies.json"
+        "yourapi/movies.json"
       );
       if (!response.ok) {
         throw new Error("Something went wrong!");
